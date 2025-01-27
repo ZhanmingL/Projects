4b6cb8828ce9002cfe73ebaa46c0721dd87c861f
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-01-26 23:25:30 -0500

    Edited way of moving input of magic stone. --Mechanical Drawing

--- Assets/Scripts/Mechanical Drawing Scripts/MagicStoneMoving.cs
+++ Assets/Scripts/Mechanical Drawing Scripts/MagicStoneMoving.cs
@@ -5,7 +5,6 @@ using static UnityEditor.PlayerSettings;
 
 public class MagicStoneMoving : MonoBehaviour
 {
-    public float StoneMove = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +14,7 @@ public class MagicStoneMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 StonePos = transform.position;
-        StonePos.x += Input.GetAxisRaw("Horizontal") * StoneMove * Time.deltaTime; //Moving horizontally by A,D,arrows keys.
-        StonePos.y += Input.GetAxisRaw("Vertical") * StoneMove * Time.deltaTime; //Moving vertically by W,S,arrows keys.
+        Vector2 StonePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //let the stone's position follow by mouse position.
         transform.position = StonePos;
     }
 }
